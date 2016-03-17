58aedf287d4e0d5357d634dad143017cc8268bd8
Adrian R. Bazaga <alu0100826456@ull.edu.es>
2016-03-17 00:28:56 +0000

    Fixed constructor test ^2

--- test/test.js
+++ test/test.js
@@ -1,12 +1,9 @@
 var expect = chai.expect;
 
-beforeEach(function() {
-  var medida = new Medida (50, 'c');
-});
-
 describe('Medida Class', function() {
   describe('Constructor', function () {
     it('Should create object correctly', function () {
+      var medida = new Medida (50, 'c');
       expect(medida.valor).to.equal(50);
     });
   });
